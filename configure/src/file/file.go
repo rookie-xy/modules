d992e8a05210ca60bc10ec736e329f4d0d4a11fb
rookie-xy <meng1985626@163.com>
2017-07-23 22:31:05 +0800

    update

--- configure/src/file/file.go
+++ configure/src/file/file.go
@@ -1,12 +1,17 @@
 package file
 
 import (
+    "os"
+    "fmt"
     "unsafe"
+
+    "github.com/fsnotify/fsnotify"
+
     "github.com/rookie-xy/worker/src/command"
     "github.com/rookie-xy/worker/src/module"
     "github.com/rookie-xy/worker/src/log"
-    "fmt"
-        "github.com/rookie-xy/worker/src/register"
+    "github.com/rookie-xy/worker/src/register"
+    "github.com/rookie-xy/worker/src/configure"
 )
 
 const Name  = "file"
@@ -29,6 +34,11 @@ var commands = []command.Item{
 
 type file struct {
     log.Log
+
+    name      string
+    size      int64
+
+    watcher  *fsnotify.Watcher
 }
 
 func New(log log.Log) module.Template {
@@ -38,18 +48,81 @@ func New(log log.Log) module.Template {
 }
 
 func (r *file) Init() {
-
-    fmt.Println("fileffffffffffffffff inittttttttttttt", path.Value.(string))
-    // 判断文件是否存在，可读性
     // 初始化文件监视器，监控配置文件
-    // 初始化解析器
+
+    resource := ""
+
+    if v := path.Value; v != nil {
+        resource = v.(string)
+    }
+
+    fileInfo, err := os.Stat(resource)
+    if err != nil {
+        if os.IsNotExist(err) {
+	    fmt.Println("a file or directory does not exist")
+
+        } else if os.IsPermission(err) {
+            fmt.Println("permission is denied")
+
+        } else {
+            fmt.Println(err)
+        }
+
+        return
+    }
+
+    r.name = resource
+    r.size = fileInfo.Size()
+
+    r.watcher, err = fsnotify.NewWatcher()
+    if err != nil {
+	fmt.Println(err)
+        return
+
+    } else {
+        r.watcher.Add(resource)
+    }
 
     return
 }
 
 func (r *file) Main() {
-fmt.Println("fileffffffffffffffff mainnnnnnnnnnnnnnnnn")
+    var char []byte
+
+    file, err := os.OpenFile(r.name, os.O_RDWR, 0777)
+    if err != nil {
+        r.Print("OpenFile error")
+    }
+
+    char = make([]byte, r.size)
+
+    if size, err := file.Read(char); err != nil {
+        if size != int(r.size) {
+            r.Print("size is not r.size")
+
+        } else {
+            r.Print(err)
+        }
+
+	return
+    }
+
+    configure.Event <- char
+
     // 发现文件变更，通知给其他模块
+    for {
+        select {
+
+        case event := <-r.watcher.Events:
+            if event.Op & fsnotify.Write == fsnotify.Write {
+                configure.Event <- char
+            }
+
+        case err := <-r.watcher.Errors:
+            r.Print(err)
+        }
+    }
+
     return
 }
 
