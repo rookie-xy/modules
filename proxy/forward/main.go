9eb1afbc21e5ae26cbfe9125f9ce241328e09cc7
Meng Shi <mengshi@meicai.cn>
2017-10-20 19:35:48 +0800

    update

--- proxy/forward/main.go
+++ proxy/forward/main.go
@@ -10,11 +10,7 @@ import (
     "github.com/rookie-xy/hubble/factory"
     "github.com/rookie-xy/hubble/state"
     "github.com/rookie-xy/hubble/proxy"
-    queue "github.com/rookie-xy/hubble/pipeline"
     "github.com/rookie-xy/hubble/plugin"
-    "github.com/rookie-xy/modules/proxy/forward/events"
-    "github.com/rookie-xy/hubble/event"
-    "github.com/rookie-xy/hubble/adapter"
     "github.com/rookie-xy/hubble/output"
 )
 
@@ -22,22 +18,20 @@ const Name  = "forward"
 
 type forward struct {
     log       log.Log
-    pipeline  queue.Queue
+//    pipeline  queue.Queue
     client    proxy.Forward
-    events   *events.Batch
     sincedb   output.Output
 }
 
 var (
-    pipeline  = command.New( plugin.Flag, "pipeline.stream",  nil, "This option use to group" )
+    batch     = command.New( module.Flag, "batch",    nil, "This option use to group" )
     client    = command.New( plugin.Flag, "client.elasticsearch",    nil, "This option use to group" )
-    batch     = command.New( module.Flag, "events",    nil, "This option use to group" )
     sincedb   = command.New( module.Flag, "sincedb",    nil, "This option use to group" )
 )
 
 var commands = []command.Item{
 
-    { pipeline,
+    { batch,
       command.FILE,
       module.Proxy,
       command.SetObject,
@@ -52,15 +46,6 @@ var commands = []command.Item{
       state.Enable,
       0,
       nil },
-
-    { batch,
-      command.FILE,
-      module.Proxy,
-      command.SetObject,
-      state.Enable,
-      0,
-      nil },
-
 
     { sincedb,
       command.FILE,
@@ -78,32 +63,13 @@ func New(l log.Log) module.Template {
 }
 
 func (r *forward) Init() {
-    key := pipeline.GetFlag() + "." + pipeline.GetKey()
-    pipeline, err := factory.Pipeline(key, r.log, pipeline.GetValue())
-    if err != nil {
-        fmt.Println("pipeline error ", err)
-        return
-    } else {
-        r.pipeline = pipeline
-    }
-
-    register.Queue(client.GetKey(), pipeline)
-
-    key = client.GetFlag() + "." + client.GetKey()
+    key := client.GetFlag() + "." + client.GetKey()
     if client, err := factory.Client(key, r.log, client.GetValue()); err != nil {
         fmt.Println("client error ", err)
         return
     } else {
         r.client = client
         register.Forword(key, client)
-    }
-
-    events := events.New(r.log)
-    if err := events.Init(batch.GetValue()); err != nil {
-        fmt.Println("events init error ", err)
-    	return
-    } else {
-        r.events = events
     }
 
     return
@@ -151,3 +117,26 @@ func (r *forward) Exit(code int) {
 func init() {
     register.Module(module.Proxy, Name, commands, New)
 }
+
+/*
+//    pipeline  = command.New( plugin.Flag, "pipeline.stream",  nil, "This option use to group" )
+    { pipeline,
+      command.FILE,
+      module.Proxy,
+      command.SetObject,
+      state.Enable,
+      0,
+      nil },
+
+
+    key := pipeline.GetFlag() + "." + pipeline.GetKey()
+    pipeline, err := factory.Pipeline(key, r.log, pipeline.GetValue())
+    if err != nil {
+        fmt.Println("pipeline error ", err)
+        return
+    } else {
+        r.pipeline = pipeline
+    }
+
+    register.Queue(client.GetKey(), pipeline)
+*/
